ae873d027f8bd7cb768435502b3f6a05ee98fde4
Shiran Naor <shiranaor@gmail.com>
2020-04-11 15:30:03 +0300

    created schema for branch

--- BE/src/Branches/branch.model.ts
+++ BE/src/Branches/branch.model.ts
@@ -1,5 +1,34 @@
 import * as mongoose from "mongoose";
 
-const BranchSchema = new mongoose.Schema({});
+const BranchSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    napa: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    district: {
+        type: String,
+        required: true
+    },
+    municipalityName: {
+        type: String,
+        required: true
+    },
+    municipalitySymbol: {
+        type: Number,
+        required: true
+    }
+});
 
 export default mongoose.model("Branch", BranchSchema);
