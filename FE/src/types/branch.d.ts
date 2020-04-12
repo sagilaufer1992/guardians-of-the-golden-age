320da27d5ea77a2f8f98a32c3ac6103cd537d636
Roni <roni123@gmail.com>
2020-04-12 11:33:15 +0300

    add fault hierarchy

---
+++ FE/src/types/branch.d.ts
@@ -0,0 +1,9 @@
+interface Branch {
+    id: string;
+    name: string; //שם נקודות החלוקה
+    address: string; // כתובת
+    napa: string; // נפה
+    district: string; // מחוז
+    municipalityName: string; // שם הרשות
+    municipalitySymbol: number; // מזהה הרשות
+  }
