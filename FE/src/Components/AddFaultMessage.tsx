d3f0ff86eb2043fbcfc4cf4ebbe267aca1f293f3
Sagi <sagi.laufer23@gmail.com>
2020-04-12 18:46:42 +0300

    validating on add fault message

--- FE/src/Components/AddFaultMessage.tsx
+++ FE/src/Components/AddFaultMessage.tsx
@@ -1,8 +1,8 @@
 import "./AddFaultMessage.scss";
 
 import React, { useState } from "react";
-import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
+import TextField from "./TextField";
 
 interface Props {
     addNewMessage: (name: string, content: string) => void;
@@ -11,31 +11,37 @@ interface Props {
 export default function AddFaultMessage({ addNewMessage }: Props) {
     const [name, setName] = useState<string>("");
     const [content, setContent] = useState<string>("");
+    const [isNameValid, setIsNameValid] = useState<boolean>(false);
+    const [isContentValid, setIsContentValid] = useState<boolean>(false);
 
     function _onSumbit() {
-        if (name !== "" && content !== "") {
-            addNewMessage(name, content);
-            setName("");
-            setContent("");
-        }
-    }
-
-    function _onNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setName(e.target.value);
-    }
-
-    function _onContentChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setContent(e.target.value);
+        addNewMessage(name, content);
+        setName("");
+        setContent("");
     }
 
     return <div className="add-message-panel">
         <div className="panel-label">פרטי הטיפול</div>
         <div className="panel-body">
-            <div className="input-label">שם המדווח על הטיפול בתקלה</div>
-            <TextField className="panel-input" variant="outlined" value={name} onChange={_onNameChange} placeholder="שם המלא לצורך תיעוד" />
-            <div className="input-label">אופן הטיפול</div>
-            <TextField className="panel-input" variant="outlined" value={content} onChange={_onContentChange} multiline rows={4} placeholder="מה הצעדים שננקטו? שים לב, ההודעה תישלח למדווח הבעיה" />
-            <Button onClick={_onSumbit} className="panel-button" variant="contained" size="small" color="primary">פרסם</Button>
+            <TextField 
+                label="שם המדווח על הטיפול בתקלה"
+                onChange={(value, isValid) => {
+                    setName(value);
+                    setIsNameValid(isValid);
+                }}
+                value={name} 
+                className="panel-input"
+                placeholder="השם המלא לצורך תיעוד" />
+            <TextField 
+                label="אופן הטיפול"
+                onChange={(value, isValid) => {
+                    setContent(value);
+                    setIsContentValid(isValid);
+                }}
+                value={content} 
+                className="panel-input"
+                placeholder="מה הצעדים שננקטו? שים לב, ההודעה תישלח למדווח הבעיה" />
+            <Button disabled={!(isNameValid && isContentValid)} onClick={_onSumbit} className="panel-button" variant="contained" size="small" color="primary">פרסם</Button>
         </div>
     </div>
 }
