50e91a1502ee50c39083b1806fb216a97aa6faf3
Git-Lior <liorkletter@gmail.com>
2020-04-13 12:35:44 +0300

    added login route

--- BE/src/Auth/auth.controller.ts
+++ BE/src/Auth/auth.controller.ts
@@ -1,3 +1,33 @@
+import User from "./auth.model";
+import { compare } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+
+const EXP_TIME = 24 * 60 * 60 * 1000;
+
 export async function getUserByToken(req, res, next) {
     res.status(200).json(req.user);
+}
+
+export async function loginUser(req, res, next) {
+    const { username, password } = req.body;
+
+    if (!username || !password) return res.status(400).send("invalid username or password");
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(400).send("invalid username or password");
+
+    const { passwordHash, _id, ...userInfo } = user.toJSON();
+
+    try {
+        const result = await compare(password, passwordHash);
+        if (!result) throw new Error();
+
+        const access_token = jwt.sign({ sub: username, exp: Date.now() + EXP_TIME }, process.env.USERS_TOKEN_SECRET, { algorithm: "HS256" });
+
+        res.status(200).json({ access_token, token_type: "bearer", ...userInfo });
+    }
+    catch (err) {
+        return res.status(400).send("invalid username or password");
+    }
 }
--- BE/src/Messages/message.controller.ts
+++ BE/src/Messages/message.controller.ts
@@ -2,7 +2,7 @@ import Message from "./message.model"
 import Fault from "../Faults/fault.model"
 
 export async function getMessages(req, res, next) {
-    if (!req.query.faultId) return res.status(400, "Must supply fault id");
+    if (!req.query.faultId) return res.status(400).send("Must supply fault id");
 
     const messages = await Message.find({ faultId: req.query.faultId });
 
@@ -13,21 +13,21 @@ export async function getMessageById(req, res, next) {
     const message = await Message.findById(req.params.id);
 
     if (!message) {
-        return res.status(404, `Message not found with id of ${req.params.id}`);
+        return res.status(404).send(`Message not found with id of ${req.params.id}`);
     }
 
     res.status(200).json(message);
 }
 
 export async function addMessage(req, res, next) {
-    if (!req.query.faultId) return res.status(400, "Must supply fault id");
+    if (!req.query.faultId) return res.status(400).send("Must supply fault id");
 
     const { token, authGroups, ...baseUser } = req.user;
 
     const fault = await Fault.findById(req.query.faultId);
 
     if (!fault) {
-        return res.status(404, `No Fault with the id of ${req.body.faultId}`);
+        return res.status(404).send(`No Fault with the id of ${req.body.faultId}`);
     }
 
     const message = await Message.create({
@@ -44,7 +44,7 @@ export async function updateMessage(req, res, next) {
     let message = await Message.findById(req.params.id);
 
     if (!Message) {
-        return res.status(404, `Message not found with id of ${req.params.id}`);
+        return res.status(404).send(`Message not found with id of ${req.params.id}`);
     }
 
     message = await Message.findOneAndUpdate({ _id: req.params.id }, req.body, {
@@ -60,7 +60,7 @@ export async function deleteMessage(req, res, next) {
     let message = await Message.findById(req.params.id);
 
     if (!message) {
-        return res.status(404, `Message not found with id of ${req.params.id}`);
+        return res.status(404).send(`Message not found with id of ${req.params.id}`);
     }
 
     message.remove();
