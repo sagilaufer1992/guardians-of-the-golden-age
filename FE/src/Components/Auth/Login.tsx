7e555a35bb4b269f690df48003e82a76d0c52d77
Shiran Naor <shiranaor@gmail.com>
2020-04-13 13:44:54 +0300

    added login page

--- FE/src/Components/Auth/Login.tsx
+++ FE/src/Components/Auth/Login.tsx
@@ -1,9 +1,45 @@
+import "./Login.scss";
 import React from "react";
 
-interface Props {
-    onLogin: (result: gg.LoginResult) => void;
-}
+import { Button, TextField, Card } from "@material-ui/core";
 
-export default function LoginPage(props: Props) {
-    return <div></div>;
+import VpnKeyRoundedIcon from "@material-ui/icons/VpnKeyRounded";
+import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+
+export default function Login() {
+    return <div className="login">
+        <Card>
+            <div className="login-container">
+                <div className="login-field">
+                    <PersonRoundedIcon />
+                    <TextField
+                        className="login-input"
+                        label="שם משתמש"
+                        variant="filled"
+                        type="password"
+                        InputProps={{
+                            disableUnderline: true,
+                        }}
+                    />
+                </div>
+                <div className="login-field">
+                    <VpnKeyRoundedIcon />
+                    <TextField
+                        className="login-input"
+                        label="סיסמה"
+                        variant="filled"
+                        type="password"
+                        InputProps={{
+                            disableUnderline: true,
+                        }}
+                    />
+                </div>
+            </div>
+        </Card>
+        <Button className="login-button">
+            <VerifiedUserIcon className="verified-icon" />
+            <span>כניסה</span>
+        </Button>
+    </div >
 }
