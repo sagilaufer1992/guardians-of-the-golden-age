b708d2fc1582bc3f19a55666c43aa48d9076c7b3
Git-Lior <liorkletter@gmail.com>
2020-04-27 23:06:21 +0300

    updated dashboard ui, add filter by deliveryType, minor fixes

--- FE/src/Components/Dashboard/DeliveryStatus.tsx
+++ FE/src/Components/Dashboard/DeliveryStatus.tsx
@@ -1,14 +1,15 @@
 import "./DeliveryStatus.scss";
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import classNames from "classnames";
 import { PieChart, Pie } from "recharts";
-import { Card, Tooltip, withStyles, Divider, Checkbox } from "@material-ui/core";
+import { Card, Tooltip, withStyles, Divider, Checkbox, Button } from "@material-ui/core";
 
 import logo from "../../assets/logo.png";
 import { isToday } from "../../utils/dates";
-import { failRasonToText } from "../../utils/translations";
+import { failRasonToText, deliveryTypeToText } from "../../utils/translations";
 
 import DeliveryReport from "../DeliveryReport/DeliveryReportDialog";
+import { DoneAll, Assessment, LocalShipping, Warning } from "@material-ui/icons";
 
 interface Props {
     date: Date;
@@ -24,7 +25,22 @@ const RADIAN = Math.PI / 180;
 
 const FAILED_COLOR = "#f44336";
 
+interface DeliveryTypeButton {
+    name: string;
+    isActive: boolean;
+}
+
 export default function DeliveryStatus({ level, levelValue, reports, hideEmpty, setHideEmpty, onDeliveryReportClick, date }: Props) {
+    const allDeliveryTypes = useMemo(() => Array.from(new Set<string>(reports.flatMap(_ => Object.keys(_.deliveries)))), [reports]);
+    const [shownDeliveryTypes, setShownDeliveryTypes] = useState<DeliveryTypeButton[]>(allDeliveryTypes.map(name => ({ name, isActive: true })));
+
+    useEffect(() => {
+        const currentTypes = shownDeliveryTypes.map(_ => _.name);
+        const newDeliveryTypes = allDeliveryTypes.filter(t => !currentTypes.includes(t)).map((name => ({ name, isActive: true })));
+
+        setShownDeliveryTypes([...shownDeliveryTypes.filter(_ => allDeliveryTypes.includes(_.name)), ...newDeliveryTypes]);
+    }, [allDeliveryTypes]);
+
     const PieChartTooltip = withStyles((theme) => ({
         tooltip: {
             backgroundColor: "white",
@@ -41,21 +57,6 @@ export default function DeliveryStatus({ level, levelValue, reports, hideEmpty, 
         return <text x={x} y={y} fill="black" fontSize={11} textAnchor="middle" dominantBaseline="central">{`${name} - ${value}`}</text>;
     }
 
-    const translateDeliveryType = (type: DeliveryType): string => {
-        switch (type) {
-            case "food_hot":
-                return "מנות חמות";
-            case "food_cold":
-                return "סלי מזון";
-            case "mask":
-                return "מסיכות";
-            case "flower":
-                return "פרחים";
-            default:
-                return type;
-        }
-    }
-
     const _generatePieChart = (color: string, data: any[]) => <PieChart height={250} width={250}>
         <Pie data={data} dataKey="value" nameKey="name" direction="rtl" fill={color} animationDuration={850} labelLine={false} innerRadius={50} label={CustomizedLabel} />
     </PieChart>;
@@ -67,15 +68,21 @@ export default function DeliveryStatus({ level, levelValue, reports, hideEmpty, 
     function singleReport(report: DeliveryReport, disabled: boolean, isTotal: boolean = false) {
         const { name, hasExternalInfo, deliveries } = report;
 
+        const filteredDeliveries = Object.entries(deliveries).filter(([key]) => shownDeliveryTypes.some(_ => _.name === key && _.isActive));
+
+        if (filteredDeliveries.length === 0) return null;
+
         return <div className="report-container" key={name}>
             <div className={classNames("location", { disabled })} onClick={() => onDeliveryReportClick(name)}>
+                {level === "municipality" && !isTotal && isToday(date) &&
+                    <DeliveryReport name={name} municipality={levelValue!} disabled={!!hasExternalInfo} />}
                 {hasExternalInfo && <Tooltip title='מכיל מידע ממערכת "משמרות הזהב"' placement="top">
                     <img className="external-logo" src={logo} />
                 </Tooltip>}
                 <span>{name}</span>
             </div>
             <div className="deliveries-data">
-                {Object.entries(deliveries).map(([deliveryType, deliveryInfo]) => {
+                {filteredDeliveries.map(([deliveryType, deliveryInfo]) => {
                     const { expected, delivered, deliveryInProgress, deliveryFailed, deliveryFailReasons, actual } = deliveryInfo;
                     const max = Math.max(actual, expected); // לפעמים הערך בפועל גדול מזה המצופה
 
@@ -88,77 +95,88 @@ export default function DeliveryStatus({ level, levelValue, reports, hideEmpty, 
                     const inProgressStyle = { width: `${inProgressPercent}%` };
 
                     return <div className="delivery">
-                        <div className="expected-text-info"><span style={{ fontWeight: "bold" }}>חלוקת {translateDeliveryType(deliveryType)}</span> | צפי יומי - {expected} אנשים</div>
+                        <div className="delivery-info">
+                            <div className="expected-text-info"><span>חלוקת {deliveryTypeToText[deliveryType] ?? deliveryType}</span> | צפי יומי - {expected} אנשים</div>
+                            <div className="actual-text-info">
+                                <span className="delivered">{delivered}<DoneAll fontSize="small" /></span>
+                                <span className="in-progress">{deliveryInProgress}<LocalShipping fontSize="small" /></span>
+                                {deliveryFailed > 0 ? <PieChartTooltip title={_generatePieChart(FAILED_COLOR, _convertToChartData(deliveryFailReasons, failRasonToText))}>
+                                    <span className="failed">{deliveryFailed}<Warning fontSize="small" /></span>
+                                </PieChartTooltip> : <span className="failed zero">{deliveryFailed}<Warning fontSize="small" /></span>}
+                                <span>{actual}<Assessment fontSize="small" /></span>
+                            </div>
+                        </div>
                         <div className="status-bar">
                             {delivered > 0 && <span className="delivered" style={deliveredStyle} />}
                             {deliveryInProgress > 0 && <span className="in-progress" style={inProgressStyle} />}
                             {deliveryFailed > 0 && <span className="failed" style={failedStyle} />}
                         </div>
-                        <div className="actual-text-info">
-                            <span className="delivered">בוצע: {delivered}</span>
-                            <span className="in-progress zero">בדרך: {deliveryInProgress}</span>
-                            {deliveryFailed > 0 ? <PieChartTooltip title={_generatePieChart(FAILED_COLOR, _convertToChartData(deliveryFailReasons, failRasonToText))}>
-                                <span className="failed"> נתקלו בבעיה: {deliveryFailed}</span>
-                            </PieChartTooltip> : <span className="failed zero">נתקלו בבעיה: {deliveryFailed}</span>}
-                            <span>סך הכל: {actual}</span>
-                        </div>                        
                     </div>;
                 })}
             </div>
-            {level === "municipality" && !isTotal && isToday(date) &&
-                <DeliveryReport name={name} municipality={levelValue!} disabled={!!hasExternalInfo} />}
         </div>
     }
 
     function getTotalReport() {
-        const initialReport: DeliveryReport = {
-            deliveries: {},
-            name: "סך הכל"
+        const totalReport: DeliveryReport = {
+            name: "סך הכל",
+            deliveries: shownDeliveryTypes.filter(_ => _.isActive).reduce((pv, { name }) => ({
+                ...pv,
+                [name]: {
+                    expected: 0,
+                    actual: 0,
+                    delivered: 0,
+                    deliveryFailed: 0,
+                    deliveryInProgress: 0,
+                    deliveryFailReasons: { address: 0, declined: 0, other: 0, unreachable: 0 },
+                }
+            }), {})
         };
 
-        const reportInfoTemplate = {
-            actual: 0,
-            delivered: 0,
-            deliveryFailReasons: {
-                address: 0,
-                declined: 0,
-                other: 0,
-                unreachable: 0,
-            },
-            deliveryFailed: 0,
-            deliveryInProgress: 0,
-            expected: 0
-        };
+        for (const report of reports) {
+            for (const [deliveryType, deliveryReport] of Object.entries(report.deliveries)) {
+                const { deliveries } = totalReport;
+                if (!deliveries[deliveryType]) continue;
 
-        return reports.reduce((accumulated, current, index, []) => {
-            Object.entries(current.deliveries).map(([deliveryType, deliveryInfo]) => {
-                if (!accumulated.deliveries[deliveryType]) accumulated.deliveries[deliveryType] = { ...reportInfoTemplate };
+                deliveries[deliveryType].actual += deliveryReport.actual;
+                deliveries[deliveryType].expected += deliveryReport.expected;
+                deliveries[deliveryType].delivered += deliveryReport.delivered;
+                deliveries[deliveryType].deliveryFailed += deliveryReport.deliveryFailed;
+                deliveries[deliveryType].deliveryInProgress += deliveryReport.deliveryInProgress;
 
-                const currentAccumulated = accumulated.deliveries[deliveryType];
+                for (const [failReason, failReasonCount] of Object.entries(deliveryReport.deliveryFailReasons)) {
+                    deliveries[deliveryType].deliveryFailReasons[failReason as FailReason] += failReasonCount;
+                }
+            }
+        }
 
-                currentAccumulated.actual += deliveryInfo.actual;
-                currentAccumulated.expected += deliveryInfo.expected;
-                currentAccumulated.delivered += deliveryInfo.delivered;
-                currentAccumulated.deliveryFailed += deliveryInfo.deliveryFailed;
-                currentAccumulated.deliveryInProgress += deliveryInfo.deliveryInProgress;
-
-                (Object.keys(deliveryInfo.deliveryFailReasons) as FailReason[]).forEach(
-                    (reason) => {
-                        currentAccumulated.deliveryFailReasons[reason] +=
-                            deliveryInfo.deliveryFailReasons[reason];
-                    }
-                );
-            });
-
-            return accumulated;
-        }, initialReport);
+        return totalReport;
     }
 
-    const reportsMemo = useMemo(() => reports.map(report => singleReport(report, level === "municipality")), [reports]);
+    const reportsMemo = useMemo(() => reports.map(report => singleReport(report, level === "municipality")).filter(Boolean), [reports, shownDeliveryTypes]);
+    const totalReportMemo = useMemo(() => reportsMemo.length > 0 ? singleReport(getTotalReport(), true, true) : null, [reports, shownDeliveryTypes]);
+
+    function onShownDeliveryTypeClick(name: string, newValue: boolean) {
+        const index = shownDeliveryTypes.findIndex(_ => _.name === name);
+        const newShownDeliveryTypes = [...shownDeliveryTypes];
+        newShownDeliveryTypes[index] = { name, isActive: newValue };
+
+        setShownDeliveryTypes(newShownDeliveryTypes);
+    }
 
     return (<Card className="panel delivery-status">
         <div className="delivery-status-title">
             <span className="status-title">סטטוס חלוקה לאנשים</span>
+            {shownDeliveryTypes.length > 0 && <div className="delivery-filter-buttons">
+                <span className="filter-buttons-title">הצג:</span>
+                {shownDeliveryTypes.map(({ name, isActive }) => <Button key={name}
+                    variant="contained"
+                    className="filter-button"
+                    color={isActive ? "primary" : "default"}
+                    onClick={() => onShownDeliveryTypeClick(name, !isActive)}>
+                    {deliveryTypeToText[name] ?? name}
+                </Button>)}
+            </div>}
             <div className="hide-empty-button">
                 <Checkbox color="primary" checked={hideEmpty} onChange={(_, v) => setHideEmpty(v)} />
                 <span>הסתר סטטוסים ריקים</span>
@@ -168,8 +186,14 @@ export default function DeliveryStatus({ level, levelValue, reports, hideEmpty, 
             לא נמצאו דיווחים בזמן וההיררכיה המבוקשים
         </div>}
         {reportsMemo.length > 0 && <>
-            <div className="total">
-                {singleReport(getTotalReport(), true, true)}
+            <div className="delivery-status-header">
+                <div className="total">{totalReportMemo}</div>
+                <Card className="delivery-status-legend">
+                    <span className="delivered"><DoneAll fontSize="small" />בוצע</span>
+                    <span className="in-progress"><LocalShipping fontSize="small" />בדרך</span>
+                    <span className="failed zero"><Warning fontSize="small" />נתקלו בבעיה</span>
+                    <span><Assessment fontSize="small" />סך הכל</span>
+                </Card>
             </div>
             <Divider variant="fullWidth" />
             <div className="all-reports">{reportsMemo}</div>
